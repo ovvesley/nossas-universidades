b732a0d08f03033a14e6e5d15ce005efc50677f7
Wesley Ferreira <ovvesley@gmail.com>
2020-11-01 16:01:57 -0300

    update server to work

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require("cors");
 
 var config = require("./config");
 
@@ -12,45 +14,46 @@ var config = require("./config");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var addUniversidadeRouter = require('./routes/add');
+var universidadeRouter = require('./routes/universidade');
 
 var app = express();
 
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 });
 
 connect
-  .then(
-    (db) => {
-      let { host, port, name } = db.connection;
+    .then(
+        (db) => {
+            let { host, port, name } = db.connection;
 
-      console.log(`Database connected correctly to Server`);
-      console.log("--------------------------------------");
-      console.log(`HOST =\t${host}; \nPORT =\t${port}; \nNAME =\t${name};`);
-      console.log("--------------------------------------");
-    },
-    (err) => {
-      console.error(
-        "❌ Database NOT connected correctly to Server: ",
-        err.message
-      );
-    }
-  )
-  .catch((err) => {
-    console.error(
-      "❌ Database NOT connected correctly to Server: ",
-      err.message
-    );
-  });
+            console.log(`Database connected correctly to Server`);
+            console.log("--------------------------------------");
+            console.log(`HOST =\t${host}; \nPORT =\t${port}; \nNAME =\t${name};`);
+            console.log("--------------------------------------");
+        },
+        (err) => {
+            console.error(
+                "❌ Database NOT connected correctly to Server: ",
+                err.message
+            );
+        }
+    )
+    .catch((err) => {
+        console.error(
+            "❌ Database NOT connected correctly to Server: ",
+            err.message
+        );
+    });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -60,21 +63,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/addUniversidade', addUniversidadeRouter);
+app.use('/universidade', universidadeRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/universidade.js
+++ routes/universidade.js
@@ -14,26 +14,25 @@ const router = express.Router();
 
 router
     .route("/")
-    .get((req, res) => {
-        res.statusCode = 404;
+    .get(async(req, res) => {
+        console.log(req.query)
+        let { regiao } = req.query;
+        console.log(regiao)
+        try {
+            let result = await Universidade.find({ regiao }).lean();
+            res.json(result)
+        } catch (error) {
+            res.statusCode = 500;
+            return res.json({
+                error: error
+            });
+        }
+    })
+    .post((req, res, next) => {
         return res.json({
             error: "not found",
             msg: "Endpoint não disponível",
         });
-    })
-    .post(async (req, res, next) => {
-        try {
-            let data = req.body
-            console.log(data)
-            let universidade = await Universidade.create({ ...data })
-            return res.json(universidade);
-        } catch (error) {
-            res.statusCode = 500;
-            return res.json({
-                error: error,
-                msg: error.statusCode,
-            });
-        }
     })
     .put((req, res) => {
         res.statusCode = 404;
