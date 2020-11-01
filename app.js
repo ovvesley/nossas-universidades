5a3e97b5a3dc05391c1ecb20e29ae0ec86c9910a
Wesley Ferreira <ovvesley@gmail.com>
2020-11-01 17:50:32 -0300

    remove interface add universidade

--- app.js
+++ app.js
@@ -62,16 +62,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/addUniversidade', addUniversidadeRouter);
+// app.use('/addUniversidade', addUniversidadeRouter);
 app.use('/universidade', universidadeRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
