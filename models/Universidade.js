abe4e817f41cd89f4e340b286dfe29f0fccb763d
Wesley Ferreira <ovvesley@gmail.com>
2020-10-31 22:37:24 -0300

    create Universidade

---
+++ models/Universidade.js
@@ -0,0 +1,39 @@
+/**
+ * Module Universidades schemas
+ * @module 
+ * @author Wesley Ferreira
+ */
+const mongoose = require("mongoose");
+
+/**
+ * UniversidadeSchema schema
+ * @class Universidade
+ */
+
+const UniversidadeSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true,
+    },
+    descricao: {
+        type: Number,
+        default: 0,
+    },
+    imagemPrincipal: {
+        type: String,
+        default: "",
+    },
+    imagens: {
+        type: Array,
+    },
+    cursos: {
+        type: Array
+    }
+});
+
+var Universidade = mongoose.model("Universidade", UniversidadeSchema);
+
+
+module.exports = Universidade;
