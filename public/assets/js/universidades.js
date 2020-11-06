bb4ca9bf0e92180ee78d51c63a35d7e190641687
Lucas <lucas-mfp@hotmail.com>
2020-11-05 21:34:40 -0300

    Adcionando os badge

--- public/assets/js/universidades.js
+++ public/assets/js/universidades.js
@@ -11,11 +11,11 @@ async function getUniversidade(regiao) {
 }
 
 
-function createCardUniversidade(UF, nome) {
+function createCardUniversidade(UF, nome, regiao) {
     return createElementFromHTML('<div class="col-lg-4 col-sm-6 branding-4 planning-4">' +
         '                            <div class="single-portfolio m-2 m-sm-t-2 m-sm-b-2 m-b-5 m-t-5">' +
         '                                <div class="card item-card card-block w-100">' +
-        `                                    <span class="card-title text-right"><strong>${UF}</strong></span>` +
+        `                                    <span class="card-title text-right"><strong class ="state-${regiao}">${UF}</strong></span>` +
         '                                    <img src="/assets/logo.png"' +
         '                                        alt="Photo of sunset">' +
         `                                    <h5 class="item-card-title mt-3 mb-3">${nome} </h5>` +
@@ -47,23 +47,23 @@ async function renderUniversidades() {
 
 
     universidadesNO.forEach(uni => {
-        var card = createCardUniversidade(uni.UF, uni.nome)
+        var card = createCardUniversidade(uni.UF, uni.nome, "norte")
         norteRow.appendChild(card)
     })
     universidadesND.forEach(uni => {
-        var card = createCardUniversidade(uni.UF, uni.nome)
+        var card = createCardUniversidade(uni.UF, uni.nome, "nordeste")
         nordesteRow.appendChild(card)
     })
     universidadesCO.forEach(uni => {
-        var card = createCardUniversidade(uni.UF, uni.nome)
+        var card = createCardUniversidade(uni.UF, uni.nome, "centro-oeste")
         centroOesteRow.appendChild(card)
     })
     universidadesSD.forEach(uni => {
-        var card = createCardUniversidade(uni.UF, uni.nome)
+        var card = createCardUniversidade(uni.UF, uni.nome, "sudeste")
         sudesteRow.appendChild(card)
     })
     universidadesSU.forEach(uni => {
-        var card = createCardUniversidade(uni.UF, uni.nome)
+        var card = createCardUniversidade(uni.UF, uni.nome, "sul")
         sulRow.appendChild(card)
     })
 
